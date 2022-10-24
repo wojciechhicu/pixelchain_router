c80040b5ad20b207f58b4b948eae9e609eb355b7
Wojciech Marek <trustme@onet.pl>
2022-10-24 15:30:42 +0200

    test

--- src/routes-tree/routes-index.ts
+++ src/routes-tree/routes-index.ts
@@ -4,7 +4,7 @@ const router = Express.Router();
 //routes import
 import routerGetConnectedNodes from './routes/get-connected-nodes';
 import routerConnectNode from './routes/connect-node';
-import routerDisconnectNode from './routes/disconnect-node'
+import routerDisconnectNode from './routes/disconnect-node';
 
 //using imports as routes
 router.use("/get-connected-nodes", routerGetConnectedNodes);
